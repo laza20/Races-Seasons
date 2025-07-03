167d2b18420008d60449ce38dbf64faaddede215
lazaro20 <salvirialazaro@gmail.com>
2025-07-03 10:57:24 -0300

    Style(validaciones.py): Modificar el orden de llegada de las variables base_de_datos y datos

--- funciones/validaciones.py
+++ funciones/validaciones.py
@@ -1,3 +1,13 @@
 from db.client import db_client
 from fastapi import HTTPException, status
 
+def validar_carga_circuito(dato, base_de_datos):
+        coleccion = getattr(db_client, base_de_datos)
+        filtros={
+            "pais_circuito"   :dato.pais_circuito,
+            "ciudad_circuito" :dato.ciudad_circuito,   
+            }
+        
+        if coleccion.find_one(filtros):
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="El circuito que desea ingresar ya esta cargado")
+        
