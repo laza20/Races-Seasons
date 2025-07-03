809c3861ac1a007e4271bcb3069842106501122c
lazaro20 <salvirialazaro@gmail.com>
2025-07-03 10:15:48 -0300

    Feat(funciones_logicas.py): Crear la funcion que permite realizar cargas de un solo documento

--- funciones/funciones_logicas.py
+++ funciones/funciones_logicas.py
@@ -8,3 +8,12 @@ def validate_object_id(id: str):
         return ObjectId(id)
     except InvalidId:
         raise HTTPException(status_code=400, detail="ID inválido")
+    
+def cargar_uno(dato, base_de_datos, schema):
+        coleccion = getattr(db_client, base_de_datos)
+        dict_dato = dict(dato)
+        del dict_dato["id"]
+        dict_dato["tipo"] = base_de_datos
+        id = coleccion.insert_one(dict_dato).inserted_id
+        new_formato = schema(coleccion.find_one({"_id":id}))
+        return new_formato
