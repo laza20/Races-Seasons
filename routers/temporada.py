e685087f9bec5e3ad0fc4a895c3cc4ff367d96a6
lazaro20 <salvirialazaro@gmail.com>
2025-07-03 11:54:00 -0300

    Feat(temporada.py): Utilizar la posibilidad de carga por parte del factor externo mediante la carpeta de funciones.

--- routers/temporada.py
+++ routers/temporada.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Path,  HTTPException, status
-from db.models.temporada import Temporada
+from db.models.temporada import Temporada, TemporadaCarga
 from db.client import db_client
 from db.schemas.temporada import temporada_schema, temporadas_schema
 from bson import ObjectId
 from bson.errors import InvalidId
+from funciones import peticiones_http_delete, peticiones_http_get, peticiones_http_post, peticiones_http_put, validaciones
 
 router = APIRouter(prefix="/Temporada",
                    tags=["Temporada"], 
@@ -15,13 +16,13 @@ def validate_object_id(id: str):
     except InvalidId:
         raise HTTPException(status_code=400, detail="ID inválido")
     
-@router.post("/Cargar/Uno", response_model= Temporada, status_code=status.HTTP_201_CREATED)
-async def create_season (temporada:Temporada):
-    dict_temporada = realizar_carga(temporada)
-    id = db_client.temporadas.insert_one(dict_temporada).inserted_id
-    new_temporada = temporada_schema(db_client.temporadas.find_one({"_id":id}))
-    
-    return Temporada(**new_temporada)
+peticiones_http_post.cargar_uno(
+    TemporadaCarga,
+    router,
+    "Temporadas",
+    temporada_schema,
+    validaciones.validar_carga_temporada
+)
 
 @router.get("/Ver/Datos")
 async def show_seasons():
@@ -31,39 +32,6 @@ async def show_seasons():
     
     return temporadas
 
-@router.post("/Cargar/Muchos", response_model=list[Temporada], status_code=status.HTTP_201_CREATED)
-async def create_many_seasons(temporadas:list[Temporada]):
-    lista_temporadas = []
-    vistas_unicas = set()
-    for temporada in temporadas:
-        clave = (temporada.categoria, temporada.year)
-        if clave in vistas_unicas:
-            raise HTTPException(
-            status_code=status.HTTP_409_CONFLICT,
-            detail="Misma temporada ingresada 2 veces"
-            )
-        vistas_unicas.add(clave)
-        dict_temporada = realizar_carga(temporada)
-        lista_temporadas.append(dict_temporada)
-        
-    resultado = db_client.temporadas.insert_many(lista_temporadas)
-    ids = resultado.inserted_ids
-    documentos = db_client.temporadas.find({"_id":{"$in": ids}})
-    news_temporadas = temporadas_schema(documentos)
-    
-    return list(news_temporadas)
 
 
-def realizar_carga(temporada):
-    filtros = {
-            "year": temporada.year,
-            "categoria": temporada.categoria
-            }
-        
-    if db_client.temporadas.find_one(filtros):
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail =f"Temporada {temporada.year} de {temporada.categoria} ya existente en la Base de datos")
-        
-    dict_temporada = dict(temporada)
-    del dict_temporada["id"]
-    dict_temporada["tipo"] = "temporada"
-    return dict_temporada
+
