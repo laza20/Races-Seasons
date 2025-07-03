363a73ef04106422e37b890d8a7f79a287a217fd
lazaro20 <salvirialazaro@gmail.com>
2025-07-03 10:27:33 -0300

    Feat(peticiones_http_post.py): Crear funcion la cual permite la carga de un documento a traves de peticiones http post

---
+++ funciones/peticiones_http_post.py
@@ -0,0 +1,13 @@
+# pyright: reportInvalidTypeForm=false
+from funciones import funciones_logicas
+from fastapi import status, Body
+from pydantic import BaseModel
+from typing import Type, Any, List
+
+def cargar_uno(Clase: Type[BaseModel], router, base_de_datos, schema, validacion):
+    @router.post("/Cargar/Uno", response_model=Clase, status_code=status.HTTP_201_CREATED)
+    async def create_one(clase: Clase = Body(...)):
+        validacion(clase, base_de_datos)
+        new_document = funciones_logicas.cargar_uno(clase, base_de_datos, schema)
+    
+        return new_document
