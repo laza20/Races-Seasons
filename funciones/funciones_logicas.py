c1c866c962cc81004872274bd916e8f0a606ee06
lazaro20 <salvirialazaro@gmail.com>
2025-07-03 10:17:25 -0300

    Feat(funciones_logicas.py): Crear la funcion que permite realizar mas de una carga de documentos

--- funciones/funciones_logicas.py
+++ funciones/funciones_logicas.py
@@ -17,3 +17,18 @@ def cargar_uno(dato, base_de_datos, schema):
         id = coleccion.insert_one(dict_dato).inserted_id
         new_formato = schema(coleccion.find_one({"_id":id}))
         return new_formato
+    
+def cargar_muchos(datos, base_de_datos , schema, validacion):
+    coleccion = getattr(db_client, base_de_datos)
+    lista = []
+    for dato in datos:
+        validacion(dato, base_de_datos)
+        dict_dato = dict(dato)
+        del dict_dato["id"]
+        dict_dato["tipo"] = base_de_datos
+        lista.append(dict_dato)
+        
+    resultado = coleccion.insert_many(lista)
+    ids = resultado.inserted_ids
+    documentos = coleccion.find({"_id":{"$in":ids}})
+    return schema(documentos)
