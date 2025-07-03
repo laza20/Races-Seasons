9dbe639708d643a254e8dd8c6fef43858c945e61
lazaro20 <salvirialazaro@gmail.com>
2025-07-03 10:55:54 -0300

    Fix(funciones_logicas.py): Eliminar el del['id'] ya que no es necesario y existen clases de carga que no cuentan con este

--- funciones/funciones_logicas.py
+++ funciones/funciones_logicas.py
@@ -12,7 +12,6 @@ def validate_object_id(id: str):
 def cargar_uno(dato, base_de_datos, schema):
         coleccion = getattr(db_client, base_de_datos)
         dict_dato = dict(dato)
-        del dict_dato["id"]
         dict_dato["tipo"] = base_de_datos
         id = coleccion.insert_one(dict_dato).inserted_id
         new_formato = schema(coleccion.find_one({"_id":id}))
