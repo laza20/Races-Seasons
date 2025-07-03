44781023c89075e37299aa6e35b926d2fc7c3dfe
lazaro20 <salvirialazaro@gmail.com>
2025-07-03 11:52:34 -0300

    Feat(validaciones.py): Crear las validaciones necesarias para realizar la carga de datos de uno o varios documentos de una temporada

--- funciones/validaciones.py
+++ funciones/validaciones.py
@@ -1,6 +1,23 @@
 from db.client import db_client
 from fastapi import HTTPException, status
         
+        
+def validar_carga_temporada(datos, base_de_datos):
+    coleccion = getattr(db_client, base_de_datos)
+    if isinstance(datos, list) and len(datos) >= 2:
+        claves = set()
+        key = (dato.year , dato.categoria.lower())
+        if key in claves:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Misma temporada ingresada 2 veces")
+        claves.add(key)
+    else:
+        # Es un único circuito
+        dato = datos if not isinstance(datos, list) else datos[0]
+
+        if coleccion.find_one({"categoria":dato.categoria, "year":dato.year}):
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail =f"Temporada {dato.year} de {dato.categoria} ya existente en la Base de datos")
+
+        
 def validar_carga_circuito(datos, base_de_datos):
     coleccion = getattr(db_client, base_de_datos)
 
@@ -25,3 +42,5 @@ def validar_carga_circuito(datos, base_de_datos):
 
         if coleccion.find_one({"pais_circuito": dato.pais_circuito, "ciudad_circuito": dato.ciudad_circuito}):
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"El circuito {dato.ciudad_circuito} ya está cargado")
+        
+
