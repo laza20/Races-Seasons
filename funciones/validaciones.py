15186e314ef8d62793678318d04ad658fc4f8777
lazaro20 <salvirialazaro@gmail.com>
2025-07-03 12:08:14 -0300

    Feat(validaciones.py): Crear las validaciones necesarias para realizar la carga de datos de uno o varios documentos de un equipo

--- funciones/validaciones.py
+++ funciones/validaciones.py
@@ -44,3 +44,23 @@ def validar_carga_circuito(datos, base_de_datos):
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"El circuito {dato.ciudad_circuito} ya está cargado")
         
 
+def validacion_carga_equipo(datos, base_de_datos):
+    coleccion = getattr(db_client, base_de_datos)
+    if isinstance(datos, list) and len(datos) >= 2:
+        equipos = set()
+        for dato in datos:
+            key = (dato.nombre_equipo.lower(), dato.pais_equipo.lower())
+            
+            if key in equipos:
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Mismo Equipo ingresado 2 veces")
+            equipos.add(key)
+            
+            if coleccion.find_one({"nombre_equipo": dato.nombre_equipo, "pais_equipo": dato.pais_equipo}):
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"El Equipo {dato.nombre_equipo} ya está cargado")
+    else:
+        # Es un único circuito
+        dato = datos if not isinstance(datos, list) else datos[0]
+
+        if coleccion.find_one({"nombre_equipo": dato.nombre_equipo, "pais_equipo": dato.pais_equipo}):
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"El Equipo {dato.nombre_equipo} ya está cargado")
+        
