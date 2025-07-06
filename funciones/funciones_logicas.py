c414dc50ae229f1eda9570e0d51857b54fe4b5ae
lazaro20 <salvirialazaro@gmail.com>
2025-07-06 19:08:41 -0300

    Feat(funciones_logicas.py): Modificar el codigo para que al ingresar un documento nuevo de sistema_de_puntuaciones.py tenga el tipo que sea ingresado por el usuario. El tipo aqui marcara si una carrera de una temporada es de determinada manera, por ejemplo en formula 1 existe el tipo sprint y el tipo principal, para cada tipo se ingresa un sistema de puntuacion distinto

--- funciones/funciones_logicas.py
+++ funciones/funciones_logicas.py
@@ -2,6 +2,7 @@ from bson import ObjectId
 from bson.errors import InvalidId
 from fastapi import HTTPException, status
 from db.client import db_client
+from typing import get_type_hints
 
 def validate_object_id(id: str):
     try:
@@ -24,10 +25,31 @@ def cargar_muchos(datos, base_de_datos , schema, validacion):
     validacion(datos, base_de_datos)
     for dato in datos:
         dict_dato = dict(dato)
-        dict_dato["tipo"] = base_de_datos
+        if base_de_datos != "Sistema_de_puntuacion":
+            dict_dato["tipo"] = base_de_datos
+        else:
+            continue
         lista.append(dict_dato)
         
     resultado = coleccion.insert_many(lista)
     ids = resultado.inserted_ids
     documentos = coleccion.find({"_id":{"$in":ids}})
     return schema(documentos)
+
+#def buscar_data(input, campo, base_de_datos):
+#    coleccion = getattr(db_client, base_de_datos)
+#    condiciones = [{campo: {"$regex": f"^{input}$", "$options": "i"}}]
+#    try:
+#        condiciones.append({"_id": ObjectId(input)})
+#    except:
+#        pass  # No es un ObjectId, no agregamos esa condición
+
+#    resultado = coleccion.find_one({"$or": condiciones})
+#    return carga_datos_faltantes(resultado, coleccion)
+
+#def carga_datos_faltantes(resultado, coleccion):
+#    if not resultado:
+#        return {"error": "No se encontraron datos"}
+#    datos_completos = resultado.copy()
+#    while True:
+        
