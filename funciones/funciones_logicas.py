8a00027e627c6abe0a18847bc4a49a1f395e84fd
lazaro20 <salvirialazaro@gmail.com>
2025-07-07 15:06:16 -0300

    Feat(funciones_logicas.py): Construccion de una funcion que permite el autocompletado por medio de la entrega de datos del usuario. Explicacion: existen tablas las cuales contienen datos ya existentes en la base de datos, por ejemplo circuitos, un circuito puede estar cargado em una o varias temporadas, lo que permite la reutilizacion de un circuito en diversas, entonces el usuario puede cargar solamente el id del circuito, el id de la temporada y el estado del circuito en la temporada y los demas datos podran ser automaticamente cargados por medio de una busqueda del circuito en la base de datos de circuitos.

--- funciones/funciones_logicas.py
+++ funciones/funciones_logicas.py
@@ -12,10 +12,22 @@ def validate_object_id(id: str):
     
 def cargar_uno(dato, base_de_datos, schema, validacion):
         coleccion = getattr(db_client, base_de_datos)
-        validacion(dato, base_de_datos)
         dict_dato = dict(dato)
         if base_de_datos != "Sistema_de_puntuacion":
             dict_dato["tipo"] = base_de_datos
+        validacion(dato, base_de_datos)
+        id = coleccion.insert_one(dict_dato).inserted_id
+        new_formato = schema(coleccion.find_one({"_id":id}))
+        return new_formato
+    
+def cargar_uno_temporada(dato, base_de_datos, schema, validacion, campo):
+        coleccion = getattr(db_client, base_de_datos)
+        dict_dato = dict(dato)
+        if base_de_datos != "Sistema_de_puntuacion":
+            dict_dato["tipo"] = base_de_datos
+        if base_de_datos in ["Equipos_por_temporada", "Pilotos_por_temporada", "Circuitos_por_temporada"]:
+            dict_dato = buscar_data(dict_dato, campo, base_de_datos)
+        validacion(dato, base_de_datos)
         id = coleccion.insert_one(dict_dato).inserted_id
         new_formato = schema(coleccion.find_one({"_id":id}))
         return new_formato
@@ -35,20 +47,51 @@ def cargar_muchos(datos, base_de_datos , schema, validacion):
     documentos = coleccion.find({"_id":{"$in":ids}})
     return schema(documentos)
 
-#def buscar_data(input, campo, base_de_datos):
-#    coleccion = getattr(db_client, base_de_datos)
-#    condiciones = [{campo: {"$regex": f"^{input}$", "$options": "i"}}]
-#    try:
-#        condiciones.append({"_id": ObjectId(input)})
-#    except:
-#        pass  # No es un ObjectId, no agregamos esa condición
+def buscar_data(dict_dato, campo, base_de_datos):
+    if campo is None or campo not in dict_dato:
+        raise HTTPException(status_code=400, detail="Falta el campo necesario para búsqueda de datos")
 
-#    resultado = coleccion.find_one({"$or": condiciones})
-#    return carga_datos_faltantes(resultado, coleccion)
+    valor = dict_dato[campo]
+    condiciones = [{campo: {"$regex": f"^{valor}$", "$options": "i"}}]
+    try:
+        condiciones.append({"_id": ObjectId(valor)})
+    except:
+        pass  # No es un ObjectId, no agregamos esa condición
 
-#def carga_datos_faltantes(resultado, coleccion):
-#    if not resultado:
-#        return {"error": "No se encontraron datos"}
-#    datos_completos = resultado.copy()
-#    while True:
+    if base_de_datos == "Equipos_por_temporada":
+        resultado = db_client.Equipos.find_one({"$or": condiciones})
+    elif base_de_datos == "Pilotos_por_temporada":
+        resultado = db_client.Pilotos.find_one({"$or": condiciones})
+    elif base_de_datos == "Circuitos_por_temporada":
+        resultado = db_client.Circuitos.find_one({"$or": condiciones})
+    else:
+        raise HTTPException(status_code=409, detail="Base de datos inválida para carga de datos faltantes")
+
+    return carga_datos_faltantes(resultado, base_de_datos, dict_dato, campo)
+
+def carga_datos_faltantes(resultado, base_de_datos, dict_dato, valor):
+    if not resultado:
+        return {"error": "No se encontraron datos"}
+    
+    temporada_oid = ObjectId(dict_dato["temporada"])
+    dato_oid = ObjectId(resultado["_id"])
+    
+    if base_de_datos == "Equipos_por_temporada":
+        dict_dato["nombre_equipo"]       = valor
+        dict_dato["pais_equipo"]         = resultado["pais_equipo"]
+        dict_dato["temporada"]           = temporada_oid 
+    elif base_de_datos == "Pilotos_por_temporada":
+        dict_dato["tipo"]                = "Piloto"
+        dict_dato["edad_piloto"]         = resultado["edad_piloto"]
+        dict_dato["nacionalidad_piloto"] = resultado["nacionalidad_piloto"]
+        dict_dato["piloto_participante"] = valor
+        dict_dato["temporada"]           = temporada_oid 
+    elif base_de_datos == "Circuitos_por_temporada":
         
+        dict_dato["circuito"]               = dato_oid
+        dict_dato["temporada"]              = temporada_oid
+        dict_dato["ciudad_circuito"]        = resultado["ciudad_circuito"]
+        dict_dato["pais_circuito"]          = resultado["pais_circuito"]
+        dict_dato["distancia_del_circuito"] = resultado["distancia_del_circuito"]
+    
+    return dict_dato
