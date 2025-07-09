8a7e4fe87cbd797259c34e1403669bf422b42618
lazaro20 <salvirialazaro@gmail.com>
2025-07-09 11:29:05 -0300

    Refactor(peticiones_http/): Crear una carpeta para separar las peticiones http lo cual permite mejorar la modularidad y facilidad de comprencion de codigo

---
+++ peticiones_http/peticiones_http_post.py
@@ -0,0 +1,31 @@
+# pyright: reportInvalidTypeForm=false
+from funciones import funciones_carga
+from fastapi import status, Body
+from pydantic import BaseModel
+from typing import Type, Any, List
+
+def cargar_uno(Clase: Type[BaseModel], router, base_de_datos, schema, validacion):
+    @router.post("/Cargar/Uno", response_model=Clase, status_code=status.HTTP_201_CREATED)
+    async def create_one(clase: Clase = Body(...)):
+        new_document = funciones_carga.cargar_uno(clase, base_de_datos, schema, validacion)
+    
+        return new_document
+    
+def cargar_uno_temporada(Clase: Type[BaseModel], router, base_de_datos, schema, validacion, campo):
+    @router.post("/Cargar/Uno/Temporada", response_model=Clase, status_code=status.HTTP_201_CREATED)
+    async def create_one_season(clase: Clase = Body(...)):
+        new_document = funciones_carga.cargar_uno_temporada(clase, base_de_datos, schema, validacion, campo)
+    
+        return new_document
+    
+def cargar_muchos(Clase: Type[BaseModel], router, base_de_datos, schema, validacion):
+    @router.post("/Cargar/Muchos", response_model=List[Clase], status_code=status.HTTP_201_CREATED)
+    async def create_many(clase: List[Clase] = Body(...)):
+        documentos = funciones_carga.cargar_muchos(clase, base_de_datos, schema, validacion)
+        return documentos
+    
+def cargar_muchos_temporada(Clase: Type[BaseModel], router, base_de_datos, schema, validacion, campo):
+    @router.post("/Cargar/Muchos/Temporada", response_model=List[Clase], status_code=status.HTTP_201_CREATED)
+    async def create_many_season(clase: List[Clase] = Body(...)):
+        documentos = funciones_carga.cargar_muchos_temporada(clase, base_de_datos, schema, validacion, campo)
+        return documentos
