8c715d37fe40c2f6d9cca1b58977749c8af903d0
lazaro20 <salvirialazaro@gmail.com>
2025-07-09 12:02:13 -0300

    Refactor(Validaciones/validar_sistema_de_puntuacion.py): Crear un archivo el cual contenga validaciones referidos a un sistema de puntuacion

---
+++ Validaciones/validar_sistema_de_puntuacion.py
@@ -0,0 +1,35 @@
+from db.client import db_client
+from fastapi import HTTPException, status
+from funciones import funciones_logicas
+
+        
+def validar_carga_sistema_de_puntuacion(datos, base_de_datos):
+    coleccion = getattr(db_client, base_de_datos)
+    if isinstance(datos, list) and len(datos) >= 2:
+        puntos = set()
+        for dato in datos:
+            temporada_oid = funciones_logicas.validate_object_id(dato.temporada)
+            key = (dato.posicion, dato.puntos, dato.tipo.capitalize(), dato.temporada)
+            
+            if key in puntos:
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Sistema de puntos ya ingresados")
+            puntos.add(key)
+            
+            if not db_client.Temporadas.find_one({"_id": temporada_oid}):
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Temporada incorrecta")
+            
+            if coleccion.find_one({"posicion": dato.posicion, "puntos": dato.puntos, "tipo": dato.tipo, "temporada":dato.temporada}):
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Sistema de puntos ya ingresados en Base de datos")
+    else:
+    
+        dato = datos if not isinstance(datos, list) else datos[0]
+        
+        temporada_oid = funciones_logicas.validate_object_id(dato.temporada)
+
+        
+        if not db_client.Temporadas.find_one({"_id": temporada_oid}):
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Temporada incorrecta")
+
+        if coleccion.find_one({"posicion": dato.posicion, "puntos": dato.puntos, "temporada":dato.temporada, "tipo": dato.tipo}):
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Sistema de puntos ya ingresados en Base de datos")
+        
