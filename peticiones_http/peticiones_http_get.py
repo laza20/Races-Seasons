008bd4ce51223b28bd212814ef0d795ce6bcc922
lazaro20 <salvirialazaro@gmail.com>
2025-07-12 13:31:32 -0300

    Feat(peticiones_http/peticiones_http_get.py): Crear una peticion la cual se encarga de traer los datos de carga de una categoria en un año (categoria, year).

--- peticiones_http/peticiones_http_get.py
+++ peticiones_http/peticiones_http_get.py
@@ -1,11 +1,15 @@
 #pyright: reportInvalidTypeForm=false
-from funciones import funciones_carga
+from funciones import funciones_carga, funciones_logicas
 from fastapi import status, Body
 from pydantic import BaseModel
 from db.client import db_client
 from typing import Type, Any, List
 from funciones import funciones_logicas
 from fastapi import APIRouter, HTTPException, status
+from db.schemas.temporada import temporada_schema
+
+
+
 
 def view_old_data(router, base_de_datos, Clase: Type[BaseModel], schema):
     @router.get("/Ver/Todo", response_model=list[Clase])
@@ -23,3 +27,30 @@ def view_data_by_id(router, base_de_datos, Clase: Type[BaseModel], schema):
             return schema(coleccion.find_one({"_id":objeto_id}))
         except:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Dato sin id o id incorrecto")
+        
+        
+def view_data_for_season_by_category_and_year(router, campo, base_de_datos, schema, base_de_datos_2):
+    @router.get("/Buscar/Datos/De/Cargas/{categoria}/{year}")
+    async def show_teams_for_load(categoria:str, year:int):
+        coleccion = getattr(db_client, base_de_datos)
+        lista=[]
+        temporada = funciones_logicas.identificar_temporada(year, categoria)
+        temporada_id = temporada["_id"]
+        temporada_oid = funciones_logicas.validate_object_id(temporada_id)
+        temporada_descripcion = funciones_logicas.transformar_de_id_a_descripcion_o_nombre(temporada_oid, "Temporadas", temporada_schema, base_de_datos_2)
+        datas = coleccion.find({"temporada":temporada_oid})
+        for data in datas:
+            obj = schema(data)
+            data_id = data["_id"]
+            data_oid = funciones_logicas.validate_object_id(data_id)
+            valor = funciones_logicas.transformar_de_id_a_descripcion_o_nombre(
+                data_oid, base_de_datos, schema, base_de_datos_2
+            )
+            dict={
+                campo : valor,
+                "temporada":temporada_descripcion,
+                "estado":obj["estado"]
+            }
+            lista.append(dict)
+        
+        return lista
