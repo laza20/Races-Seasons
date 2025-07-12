90d3e3c01b22615f5b4b3ba04641fe31a80d3385
lazaro20 <salvirialazaro@gmail.com>
2025-07-12 13:24:57 -0300

    Feat(funciones/funciones_logicas.py): Crear una funcion la cual se encarga de identificar una temporada

--- funciones/funciones_logicas.py
+++ funciones/funciones_logicas.py
@@ -3,6 +3,8 @@ from bson.errors import InvalidId
 from fastapi import HTTPException, status
 from db.client import db_client
 from typing import get_type_hints
+from db.schemas.temporada import temporada_schema
+
 
 def validate_object_id(id: str):
     try:
@@ -15,3 +17,11 @@ def validate_object_id_or_false(id: str):
         return ObjectId(id)
     except InvalidId:
         return False
+    
+def identificar_temporada(year, categoria):
+        temporada = temporada_schema(db_client.Temporadas.find_one({"year":year,"categoria":categoria}))
+        if not temporada:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="temporada incorrecta")
+        return temporada
+    
+
