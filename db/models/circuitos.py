a82bf7c2071df1570bae6a4c91ea34d9dba56858
lazaro20 <salvirialazaro@gmail.com>
2025-07-12 14:31:54 -0300

    Refatcor(db/models/circuitos.py): Modificar las posiciones de las propiedades de circuitos en las clases para tener mejor recibimiento de datos en las llamadas

--- db/models/circuitos.py
+++ db/models/circuitos.py
@@ -2,16 +2,16 @@ from pydantic import BaseModel
 
 class Circuitos(BaseModel):
     id                       : str | None = None
-    pais_circuito            : str
     ciudad_circuito          : str
+    pais_circuito            : str
     distancia_del_circuito   : float
     sistema_medicion         : str
     tipo                     : str | None = None
     estado                   : bool
     
 class CircuitosCarga(BaseModel):
-    pais_circuito            : str
     ciudad_circuito          : str
+    pais_circuito            : str
     distancia_del_circuito   : float
     sistema_medicion         : str
     tipo                     : str | None = None
@@ -20,8 +20,8 @@ class CircuitosCarga(BaseModel):
 class CircuitosPorTemporada(BaseModel):
     id                       : str   | None = None
     circuito                 : str
-    pais_circuito            : str   | None = None
     ciudad_circuito          : str   | None = None
+    pais_circuito            : str   | None = None
     distancia_del_circuito   : float | None = None
     temporada                : str
     tipo                     : str   | None = None
