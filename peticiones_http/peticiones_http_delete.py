f25581ad3314eb6f010512e2d51c79cf4d2d9cbe
lazaro20 <salvirialazaro@gmail.com>
2025-07-15 10:30:36 -0300

    Feat(peticiones_http/peticiones_http_delete.py): Crear una funcion la cual se encarge de borrar un documento por medio del ID.

--- peticiones_http/peticiones_http_delete.py
+++ peticiones_http/peticiones_http_delete.py
@@ -2,6 +2,7 @@
 from fastapi import status
 from db.client import db_client
 from fastapi import  HTTPException, status
+from funciones import funciones_logicas
 
 def delete_old_by_type(router, base_de_datos):
     @router.delete("/Borrar/Todo", status_code=status.HTTP_202_ACCEPTED)
@@ -10,3 +11,12 @@ def delete_old_by_type(router, base_de_datos):
         borrado = coleccion.delete_many({"tipo":base_de_datos})
         if not borrado:
             raise HTTPException(status_code=404, detail="Datos no encontrados")
+        
+def delete_one_by_id(router, base_de_datos):
+    @router.delete("/Borrar/Por/{id}", status_code=status.HTTP_204_NO_CONTENT)
+    async def delete_equipo(id:str):
+        coleccion = getattr(db_client, base_de_datos)
+        oid = funciones_logicas.validate_object_id(id)
+        borrado = coleccion.find_one_and_delete({"_id": oid})
+        if not borrado:
+            raise HTTPException(status_code=404, detail="Equipo no encontrado")
