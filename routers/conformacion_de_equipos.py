7d620f5910b3abadd25f742f72fc4e94b4f705fb
lazaro20 <salvirialazaro@gmail.com>
2025-07-16 10:15:36 -0300

    Feat(routers/conformacion_de_equipos.py): Agregar la funcion que posibilita ver un documento mediante la entrega de un dato (en este caso el nombre del equipo).

--- routers/conformacion_de_equipos.py
+++ routers/conformacion_de_equipos.py
@@ -28,6 +28,14 @@ peticiones_http_post.cargar_muchos(
     validar_conformacion_de_equipos.validar_carga_de_conformacion_de_equipos    
 )
 
+lista_de_propiedades_str_sing = ["nombre_equipo"]
+
+peticiones_http_get.view_one_document_for_data_str(
+    router, 
+    "Conformacion_de_equipos", 
+    piloto_x_equipo_schema, 
+    lista_de_propiedades_str_sing
+    )
 
 peticiones_http_get.view_old_data(
     router, 
