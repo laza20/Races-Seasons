436a02159965e41502181f21708691ad6958cb6b
lazaro20 <salvirialazaro@gmail.com>
2025-07-17 14:58:06 -0300

    Docts(back ups/): Modificar los back ups para mejorar su comprencion.

---
+++ back ups/Sistemas de puntuacion/Sistema de puntuacion Moto gp 2025.js
@@ -0,0 +1,220 @@
+[
+  {
+    "id": "68793933d2a8637ae1363ccf",
+    "posicion": 1,
+    "puntos": 25,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cd0",
+    "posicion": 2,
+    "puntos": 20,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cd1",
+    "posicion": 3,
+    "puntos": 16,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cd2",
+    "posicion": 4,
+    "puntos": 13,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cd3",
+    "posicion": 5,
+    "puntos": 11,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cd4",
+    "posicion": 6,
+    "puntos": 10,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cd5",
+    "posicion": 7,
+    "puntos": 9,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cd6",
+    "posicion": 8,
+    "puntos": 8,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cd7",
+    "posicion": 9,
+    "puntos": 7,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cd8",
+    "posicion": 10,
+    "puntos": 6,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cd9",
+    "posicion": 11,
+    "puntos": 5,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cda",
+    "posicion": 12,
+    "puntos": 4,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cdb",
+    "posicion": 13,
+    "puntos": 3,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cdc",
+    "posicion": 14,
+    "puntos": 2,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cdd",
+    "posicion": 15,
+    "puntos": 1,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Principal",
+    "estado": true
+  }]
+  
+  [
+  {
+    "id": "68793933d2a8637ae1363cde",
+    "posicion": 1,
+    "puntos": 12,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Sprint",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363cdf",
+    "posicion": 2,
+    "puntos": 9,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Sprint",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363ce0",
+    "posicion": 3,
+    "puntos": 7,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Sprint",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363ce1",
+    "posicion": 4,
+    "puntos": 6,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Sprint",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363ce2",
+    "posicion": 5,
+    "puntos": 5,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Sprint",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363ce3",
+    "posicion": 6,
+    "puntos": 4,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Sprint",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363ce4",
+    "posicion": 7,
+    "puntos": 3,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Sprint",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363ce5",
+    "posicion": 8,
+    "puntos": 2,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Sprint",
+    "estado": true
+  },
+  {
+    "id": "68793933d2a8637ae1363ce6",
+    "posicion": 9,
+    "puntos": 1,
+    "temporada": "68792f93dced9f796ea38d43",
+    "tipo": "Sistema_de_puntuacion",
+    "tipo_carrera": "Sprint",
+    "estado": true
+  }
+]
