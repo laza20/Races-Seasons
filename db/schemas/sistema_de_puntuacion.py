c65c38a2bd2bd87a1ae987c1b84706a39599ed76
lazaro20 <salvirialazaro@gmail.com>
2025-07-17 11:06:36 -0300

    Feat(db/schemas/sistema_de_puntuacion.py): Crear 2 schemas nuevos, uno se encargara de mostrar el sistema de puntuacion en su formato de carga y el otro de mostrar todos los formatos de carga que pida el usuario a travez de un filtro.

--- db/schemas/sistema_de_puntuacion.py
+++ db/schemas/sistema_de_puntuacion.py
@@ -7,6 +7,16 @@ def punto_schema(punto)->dict:
             "tipo"                    : str(punto["tipo"]),
             "estado"                  : bool(punto["estado"])}
         
+def punto_temporada_schema(punto)->dict:
+        return {
+            "posicion"                : int(punto["posicion"]),
+            "puntos"                  : int(punto["puntos"]),
+            "temporada"               : str(punto["temporada"]),
+            "tipo"                    : str(punto["tipo"]),
+            "estado"                  : bool(punto["estado"])}
         
 def puntos_schema(puntos)->list:
+    return [punto_schema(punto) for punto in puntos]
+
+def puntos_temporada_schema(puntos)->list:
     return [punto_schema(punto) for punto in puntos]
