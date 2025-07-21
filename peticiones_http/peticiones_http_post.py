577cfdfd954dd12648410bb0c82517fa35270ca7
lazaro20 <salvirialazaro@gmail.com>
2025-07-21 09:14:16 -0300

    Feat(peticiones_http/peticiones_http_post.py): Crear una peticion http post la cual se encarga de los datos de una carrera (un solo documento, es decir, una sola posicion).

--- peticiones_http/peticiones_http_post.py
+++ peticiones_http/peticiones_http_post.py
@@ -1,8 +1,9 @@
 # pyright: reportInvalidTypeForm=false
-from funciones import funciones_carga
+from funciones import funciones_carga, funcion_carga_carrera
 from fastapi import status, Body
 from pydantic import BaseModel
 from typing import Type, Any, List
+from db.models.carrera_todos_los_datos import DatosTotales
 
 def cargar_uno(Clase: Type[BaseModel], router, base_de_datos, schema, validacion):
     @router.post("/Cargar/Uno", response_model=Clase, status_code=status.HTTP_201_CREATED)
@@ -16,6 +17,17 @@ def cargar_uno_temporada(Clase: Type[BaseModel], router, base_de_datos, schema, 
     async def create_one_season(clase: Clase = Body(...)):
         new_document = funciones_carga.cargar_uno_temporada(clase, base_de_datos, schema, validacion, campo)
     
+        return new_document
+    
+def cargar_uno_carrera(Clase: Type[BaseModel], router, base_de_datos, schema, validacion):
+    @router.post("/Cargar/Uno", response_model=DatosTotales, status_code=status.HTTP_201_CREATED)
+    async def create_one(clase: Clase = Body(...)):
+        new_document = await funcion_carga_carrera.cargar_carrera(
+            clase,
+            base_de_datos,
+            schema,
+            validacion
+        )
         return new_document
     
 def cargar_muchos(Clase: Type[BaseModel], router, base_de_datos, schema, validacion):
