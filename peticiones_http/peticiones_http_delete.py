9fa4a3b54dacd32af93812c1cec01725d06eb45b
lazaro20 <salvirialazaro@gmail.com>
2025-07-23 16:07:03 -0300

    Feat(peticiones_http/peticiones_http_delete.py): Crear una funcion para eliminar los datos de manera masiva sin tener que eliminar todos.

--- peticiones_http/peticiones_http_delete.py
+++ peticiones_http/peticiones_http_delete.py
@@ -20,3 +20,17 @@ def delete_one_by_id(router, base_de_datos):
         borrado = coleccion.find_one_and_delete({"_id": oid})
         if not borrado:
             raise HTTPException(status_code=404, detail="Equipo no encontrado")
+        
+def delete_many_by_data_str(router, base_de_datos, lista_datos):
+    @router.delete("/Borrar/Por/Data/{data}", status_code=status.HTTP_204_NO_CONTENT)
+    async def delete_by_data(data:str):
+        encontrado = "False"
+        coleccion = getattr(db_client, base_de_datos)
+        for dato in lista_datos:
+            datos = coleccion.find_one({dato:data})
+            coleccion.delete_many({dato:data})
+            if datos:
+                encontrado = "True"
+        
+        if encontrado != "True":   
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"No se ah encontrado ningun dato {data}, con la propiedad {dato} en los documentos {base_de_datos}")
