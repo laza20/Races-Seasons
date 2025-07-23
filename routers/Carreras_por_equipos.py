1fb55f65669805de17cc13d44e18048dd6bc01d3
lazaro20 <salvirialazaro@gmail.com>
2025-07-23 15:20:55 -0300

    Feat(routers/Carreras_por_equipos.py): Crear un router para poder manipular los datos que son introducidos en la tabla carreras_por_equipo cuando se realiza una carga.

---
+++ routers/Carreras_por_equipos.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+from db.schemas.puntos_por_equipos import puntos_por_equipo_schema, puntos_por_equipos_schema
+from db.models.puntos_por_equipos import PuntosXEquipo
+from peticiones_http import peticiones_http_delete, peticiones_http_get, peticiones_http_put
+
+router = APIRouter(prefix="/Carreras/Por/Equipos",
+                   tags=["Carreras por piloto"],
+                   responses={404:{"Message":"No encontrado"}}
+)
+
