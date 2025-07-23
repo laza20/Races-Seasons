6b58178fd03ac394cd6758507f527728856da818
lazaro20 <salvirialazaro@gmail.com>
2025-07-23 15:27:49 -0300

    Style(routers/carreras.py): Eliminar importaciones y funciones que no van a ser tenidas en cuenta en este caso para facilitar la comprencion del codigo.

--- routers/carreras.py
+++ routers/carreras.py
@@ -1,13 +1,6 @@
-from fastapi import APIRouter, HTTPException, status,Path
-from db.client import db_client
-from bson import ObjectId
-from bson.errors import InvalidId
-from db.models.carreras import Carreras, CarrerasCarga
-from db.models.carrera_todos_los_datos import DatosTotales
+from fastapi import APIRouter
+from db.models.carreras import Carreras
 from db.schemas.carreras import carrera_schema , carreras_schema
-from db.schemas.puntos_por_equipos import puntos_por_equipo_schema, puntos_por_equipos_schema
-from db.schemas.puntos_por_piloto import puntos_por_piloto_schema, puntos_por_pilotos_schema
-from db.schemas.temporada import temporada_schema, temporadas_schema
 from peticiones_http import peticiones_http_delete, peticiones_http_get, peticiones_http_post, peticiones_http_put
 from Validaciones import validar_carreras_por_temporada
 
@@ -18,12 +11,6 @@ router = APIRouter(prefix="/Carreras",
                    responses={404:{"Message":"No encontrado"}}
 )
 
-
-def validate_object_id(id: str):
-    try:
-        return ObjectId(id)
-    except InvalidId:
-        raise HTTPException(status_code=400, detail="ID inválido")
     
 peticiones_http_post.cargar_uno_carrera(
     Carreras,  
@@ -48,28 +35,4 @@ peticiones_http_get.view_old_data(
     carreras_schema    
 )
 
-
-
-@router.get("/Carga/{ciudad_circuito}/{year}/{categoria}/{tipo}", response_model=list[CarrerasCarga])
-async def show_race_for_load (categoria:str, year:int, ciudad_circuito:str, tipo:str):
-    temporada = db_client.temporadas.find_one({"categoria":categoria, "year":year})
-    id = temporada["_id"]
-    temporada_oid = validate_object_id(id)
-    datas = carreras_schema(db_client.carreras.find({"temporada":temporada_oid, 
-                                                     "ciudad_circuito":ciudad_circuito, 
-                                                     "tipo":tipo}))
-    
-    posiciones = sorted(datas, key=lambda c: c["posicion"])
-        
-    return posiciones
-
-     
-@router.delete("/borrar/carreras/tipo/{ciudad_circuito}/{tipo}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_circuito(ciudad_circuito:str, tipo:str):
-    borrado = db_client.carreras.delete_many({"ciudad_circuito":ciudad_circuito, "tipo":tipo})
-    borrado = db_client.puntos_por_equipo.delete_many({"ciudad_circuito":ciudad_circuito, "tipo":tipo})
-    borrado = db_client.puntos_por_piloto.delete_many({"ciudad_circuito":ciudad_circuito, "tipo":tipo})
-    
-    if not borrado:
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="No se encontro el circuito que se desea eliminar")
 
