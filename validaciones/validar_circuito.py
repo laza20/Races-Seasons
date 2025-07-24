b0e8f5dbe6ad7fd01ebcd44fd10fae8dac938cc5
lazaro20 <salvirialazaro@gmail.com>
2025-07-24 14:42:37 -0300

    Feat(validaciones/validar_circuito.py): Crear una funcion secundaria la cual permita no tener codigo duplicado.

--- validaciones/validar_circuito.py
+++ validaciones/validar_circuito.py
@@ -11,21 +11,21 @@ def validar_carga_circuito(datos, base_de_datos):
     if isinstance(datos, list) and len(datos) >= 2:
         ciudades = set()
         for dato in datos:
-            key = (dato.pais_circuito.lower(), dato.ciudad_circuito.lower())
-            
-            # Verificar duplicados en la misma carga
+            key = validar_carga_circuito_2(dato)
             if key in ciudades:
                 raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Mismo circuito ingresado 2 veces")
             ciudades.add(key)
-
-            # Verificar si ya existe en la base de datos
-            if coleccion.find_one({"pais_circuito": dato.pais_circuito, "ciudad_circuito": dato.ciudad_circuito}):
-                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"El circuito {dato.ciudad_circuito} ya está cargado")
-
     else:
         # Es un único circuito
         dato = datos if not isinstance(datos, list) else datos[0]
+        key = validar_carga_circuito_2(dato)
 
-        if coleccion.find_one({"pais_circuito": dato.pais_circuito, "ciudad_circuito": dato.ciudad_circuito}):
+        
+        
+def validar_carga_circuito_2(dato):
+    key = (dato.pais_circuito.lower(), dato.ciudad_circuito.lower())
+    
+    if db_client.Circuitos.find_one({"pais_circuito": dato.pais_circuito, "ciudad_circuito": dato.ciudad_circuito}):
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"El circuito {dato.ciudad_circuito} ya está cargado")
         
+    return key
