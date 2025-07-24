8b389e9cc48596bf2a1d0e636cc450ae1dab2760
lazaro20 <salvirialazaro@gmail.com>
2025-07-24 14:45:31 -0300

    Feat(validaciones/validar_equipo.py): Crear una funcion secundaria la cual permita no tener codigo duplicado.

--- validaciones/validar_equipo.py
+++ validaciones/validar_equipo.py
@@ -3,21 +3,24 @@ from fastapi import HTTPException, status
 
 
 def validacion_carga_equipo(datos, base_de_datos):
-    coleccion = getattr(db_client, base_de_datos)
     if isinstance(datos, list) and len(datos) >= 2:
         equipos = set()
         for dato in datos:
-            key = (dato.nombre_equipo.lower(), dato.pais_equipo.lower())
+            
+            key = validacion_carga_equipo_2(dato)
             
             if key in equipos:
                 raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Mismo Equipo ingresado 2 veces")
             equipos.add(key)
-            
-            if coleccion.find_one({"nombre_equipo":{"$regex": f"^{dato.nombre_equipo}$", "$options": "i"}, "pais_equipo": {"$regex": f"^{dato.pais_equipo}$", "$options": "i"}}):
-                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"El Equipo {dato.nombre_equipo} ya está cargado")
     else:
         dato = datos if not isinstance(datos, list) else datos[0]
-
-        if coleccion.find_one({"nombre_equipo":{"$regex": f"^{dato.nombre_equipo}$", "$options": "i"}, "pais_equipo": {"$regex": f"^{dato.pais_equipo}$", "$options": "i"}}):
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"El Equipo {dato.nombre_equipo} ya está cargado")
+        key = validacion_carga_equipo_2(dato)
+        
+def validacion_carga_equipo_2(dato):
+    key = (dato.nombre_equipo.lower(), dato.pais_equipo.lower())
+    
+    if db_client.Equipos.find_one({"nombre_equipo":{"$regex": f"^{dato.nombre_equipo}$", "$options": "i"}, "pais_equipo": {"$regex": f"^{dato.pais_equipo}$", "$options": "i"}}):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"El Equipo {dato.nombre_equipo} ya está cargado")
+    
+    return key
         
