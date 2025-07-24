ecda9f74ea8f241c1cbb9a038709344704f6237e
lazaro20 <salvirialazaro@gmail.com>
2025-07-24 18:20:30 -0300

    Feat(validaciones_generales/validaciones_generales.py): Agregar una funcion que permita verificar si un dato no esta en una base de datos. Ademas agregar una serie de parametros los cuales permitan verificar si un dato (siendo str) se ingresa si importar el tipo de letra se cerifica de manera correcta.

--- validaciones_generales/validaciones_generales.py
+++ validaciones_generales/validaciones_generales.py
@@ -22,11 +22,41 @@ def validacion_simple(base_de_datos, dato):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"No hay definición de campos para la colección {base_de_datos}")
     
     campos = listas_de_campos[base_de_datos]
-
+    
     for campo in campos:
-        if coleccion.find_one({campo:dato}):
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"El dato {dato} ya se encuentra en la base de datos {campos}, coincidio con un dato de {campo}")
+        query = (
+            {campo: {"$regex": f"^{dato}$", "$options": "i"}} if isinstance(dato, str)
+            else {campo: dato}
+        )
+        if coleccion.find_one(query):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"El dato '{dato}' ya se encuentra en la base de datos '{base_de_datos}' en el campo '{campo}'"
+            )
         
+        
+def validacion_simple_negativa(base_de_datos, dato):
+    coleccion = getattr(db_client, base_de_datos)
+    
+    if base_de_datos not in listas_de_campos:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"No hay definición de campos para la colección {base_de_datos}")
+    
+    campos = listas_de_campos[base_de_datos]
+    
+    for campo in campos:
+        query = (
+            {campo: {"$regex": f"^{dato}$", "$options": "i"}} if isinstance(dato, str)
+            else {campo: dato}
+        )
+        if coleccion.find_one(query):
+            return
+        
+        raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"El dato '{dato}' no se encuentra en la base de datos '{base_de_datos}'"
+            )
+
+
 
 def validacion_doble(base_de_datos, dato_uno, dato_dos):
     coleccion = getattr(db_client, base_de_datos)
@@ -40,5 +70,14 @@ def validacion_doble(base_de_datos, dato_uno, dato_dos):
         for j,campo_dos in enumerate(campos):
             if i == j:
                 continue
-            if coleccion.find_one({campo_uno:dato_uno, campo_dos:dato_dos}):
+            
+            query_1 = (
+            {campo_uno: {"$regex": f"^{dato_uno}$", "$options": "i"}} if isinstance(dato_uno, str)
+            else {campo_uno: dato_uno}
+            )
+            query_2 = (
+            {campo_dos: {"$regex": f"^{dato_dos}$", "$options": "i"}} if isinstance(dato_dos, str)
+            else {campo_dos: dato_dos}
+            )
+            if coleccion.find_one({**query_1, **query_2}):
                 raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"El documento con los datos ({dato_uno} y {dato_dos}) ya se encuentra en la base de datos de {base_de_datos}")
