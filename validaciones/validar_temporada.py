9b0228585f9fe0636104604b205fab8c2cdc632f
lazaro20 <salvirialazaro@gmail.com>
2025-07-24 17:55:52 -0300

    Feat(validaciones/validar_temporada.py): Crear una funcion la cual divida la funcion y permita no repetir codigo.

--- validaciones/validar_temporada.py
+++ validaciones/validar_temporada.py
@@ -8,13 +8,16 @@ def validar_carga_temporada(datos, base_de_datos):
     if isinstance(datos, list) and len(datos) >= 2:
         claves = set()
         for dato in datos:
-            key = (dato.year , dato.categoria.lower())
-            validaciones_generales.validacion_doble(base_de_datos, dato.categoria, dato.year)
+            key  = validar_carga_temporada_2(dato, base_de_datos)
             if key in claves:
                 raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Misma temporada ingresada 2 veces")
             claves.add(key)
     else:
         # Es un único circuito
         dato = datos if not isinstance(datos, list) else datos[0]
-    
-        validaciones_generales.validacion_doble(base_de_datos, dato.categoria, dato.year)
+        key  = validar_carga_temporada_2(dato, base_de_datos)
+        
+def validar_carga_temporada_2(dato, base_de_datos):
+    key = (dato.year , dato.categoria.lower())
+    validaciones_generales.validacion_doble(base_de_datos, dato.categoria, dato.year)
+    return key
