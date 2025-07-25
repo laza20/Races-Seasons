62abc58478d6b06c45d85df1bfdabd8256ad0bb3
lazaro20 <salvirialazaro@gmail.com>
2025-07-25 14:00:47 -0300

    Refactor and Feat(validaciones_generales/validaciones_generales_dobles.py): Crear las funciones para validaciones negativas, en caso de ser dos str, un str o sin str, esto se hace por medio de una funcion orquestadora. Ademas utilizar la posibilidad de mensajes http por medio de llamadas para mejorar la modularizacion del codigo.

--- validaciones_generales/validaciones_generales_dobles.py
+++ validaciones_generales/validaciones_generales_dobles.py
@@ -1,13 +1,10 @@
 from db.client import db_client
-from fastapi import HTTPException, status
 from validaciones_generales.lista_de_campos import listas_de_campos
+from errores import errores_dobles, errores_simples
 
 def validacion_doble_general(base_de_datos, dato_uno, dato_dos):
     if base_de_datos not in listas_de_campos:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"No hay definición de campos para la colección {base_de_datos}"
-        )
+        errores_simples.error_sin_base_de_datos(base_de_datos)
     coleccion = getattr(db_client, base_de_datos)
     campos = listas_de_campos[base_de_datos]
     
@@ -32,10 +29,7 @@ def validacion_doble_str(coleccion,campos, base_de_datos, dato_uno, dato_dos):
             }
             
             if coleccion.find_one(query):
-                raise HTTPException(
-                    status_code=status.HTTP_409_CONFLICT,
-                    detail=f"Ya existe un documento en '{base_de_datos}' con {campo_uno} = '{dato_uno}' y {campo_dos} = '{dato_dos}'"
-                )
+                errores_dobles.error_doble_positivo(base_de_datos, campo_uno, campo_dos, dato_uno, dato_dos)
     
 def validacion_doble_one_str(coleccion,campos, base_de_datos, dato_uno, dato_dos):
     
@@ -50,10 +44,7 @@ def validacion_doble_one_str(coleccion,campos, base_de_datos, dato_uno, dato_dos
                 }
             
                 if coleccion.find_one(query):
-                    raise HTTPException(
-                    status_code=status.HTTP_409_CONFLICT,
-                    detail=f"Ya existe un documento en '{base_de_datos}' con {campo_uno} = '{dato_uno}' y {campo_dos} = '{dato_dos}'"
-                    )
+                    errores_dobles.error_doble_positivo(base_de_datos, campo_uno, campo_dos, dato_uno, dato_dos)
             else:
                 query = {
                 campo_uno: dato_uno,
@@ -61,10 +52,7 @@ def validacion_doble_one_str(coleccion,campos, base_de_datos, dato_uno, dato_dos
                 }
                 
                 if coleccion.find_one(query):
-                    raise HTTPException(
-                    status_code=status.HTTP_409_CONFLICT,
-                    detail=f"Ya existe un documento en '{base_de_datos}' con {campo_uno} = '{dato_uno}' y {campo_dos} = '{dato_dos}'"
-                    )
+                    errores_dobles.error_doble_positivo(base_de_datos, campo_uno, campo_dos, dato_uno, dato_dos)
 
 
 
@@ -80,7 +68,84 @@ def validacion_doble_no_str(coleccion,campos, base_de_datos, dato_uno, dato_dos)
             }
 
             if coleccion.find_one(query):
-                raise HTTPException(
-                    status_code=status.HTTP_409_CONFLICT,
-                    detail=f"Ya existe un documento en '{base_de_datos}' con {campo_uno} = '{dato_uno}' y {campo_dos} = '{dato_dos}'"
-                )
+                errores_dobles.error_doble_positivo(base_de_datos, campo_uno, campo_dos, dato_uno, dato_dos)
+
+
+def validacion_doble_negativa_general(base_de_datos, dato_uno, dato_dos):
+    if base_de_datos not in listas_de_campos:
+        errores_simples.error_sin_base_de_datos(base_de_datos)
+    coleccion = getattr(db_client, base_de_datos)
+    campos = listas_de_campos[base_de_datos]
+    
+    if isinstance(dato_uno, str) and isinstance(dato_dos, str):
+        validacion_doble_negativa_str(coleccion,campos, base_de_datos, dato_uno, dato_dos)
+    elif isinstance(dato_uno, str) or isinstance(dato_dos, str):
+        validacion_doble_negativa_one_str(coleccion,campos, base_de_datos, dato_uno, dato_dos)
+    else:
+        validacion_doble_negativa_no_str(coleccion,campos, base_de_datos, dato_uno, dato_dos)
+        
+        
+def validacion_doble_negativa_str(coleccion,campos, base_de_datos, dato_uno, dato_dos):
+    for i, campo_uno in enumerate(campos):
+        for j, campo_dos in enumerate(campos):
+            if i == j:
+                continue  # Evita comparar el mismo campo contra sí mismo
+            
+            query = {
+                campo_uno: {"$regex": f"^{dato_uno}$", "$options": "i"},
+                campo_dos:{"$regex": f"^{dato_dos}$", "$options": "i"}
+            }
+            
+            if coleccion.find_one(query):
+                return
+                
+    errores_dobles.error_doble_negativo(base_de_datos, dato_uno, dato_dos)
+    
+    
+
+def validacion_doble_negativa_one_str(coleccion,campos, base_de_datos, dato_uno, dato_dos):
+    for i, campo_uno in enumerate(campos):
+        for j, campo_dos in enumerate(campos):
+            if i == j:
+                continue  # Evita comparar el mismo campo contra sí mismo
+            if isinstance(dato_uno, str):
+                query = {
+                campo_uno: {"$regex": f"^{dato_uno}$", "$options": "i"},
+                campo_dos: dato_dos
+                }
+            
+                if coleccion.find_one(query):
+                    return
+            else:
+                query = {
+                campo_uno: dato_uno,
+                campo_dos: {"$regex": f"^{dato_dos}$", "$options": "i"},
+                }
+                
+                if coleccion.find_one(query):
+                    return
+                    
+                    
+    errores_dobles.error_doble_negativo(base_de_datos, dato_uno, dato_dos)
+
+
+
+
+
+
+
+def validacion_doble_negativa_no_str(coleccion,campos, base_de_datos, dato_uno, dato_dos):
+    for i, campo_uno in enumerate(campos):
+        for j, campo_dos in enumerate(campos):
+            if i == j:
+                continue  # Evita comparar el mismo campo contra sí mismo
+            
+            query = {
+                campo_uno : dato_uno,
+                campo_dos : dato_dos
+            }
+
+            if coleccion.find_one(query):
+                return
+            
+    errores_dobles.error_doble_negativo(base_de_datos, dato_uno, dato_dos)
