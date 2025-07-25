775c72fb672af11d5ec7739d63454a3b6712184f
lazaro20 <salvirialazaro@gmail.com>
2025-07-25 11:20:28 -0300

    Fix(validaciones_generales/validaciones_generales_simples.py): Corregir las llamadas a los errores.

--- validaciones_generales/validaciones_generales_simples.py
+++ validaciones_generales/validaciones_generales_simples.py
@@ -1,8 +1,9 @@
 from db.client import db_client
 from fastapi import HTTPException, status
 from validaciones_generales.lista_de_campos import listas_de_campos
+from errores import errores_simples
 
-def validacion_simple_str(base_de_datos, dato):
+def validacion_simple_general(base_de_datos, dato):
     coleccion = getattr(db_client, base_de_datos)
     
     if base_de_datos not in listas_de_campos:
@@ -10,35 +11,61 @@ def validacion_simple_str(base_de_datos, dato):
     
     campos = listas_de_campos[base_de_datos]
     
+    if isinstance(dato, str):
+        validacion_simple_str(coleccion, campos, base_de_datos, dato)
+    else:
+        validacion_simple_no_str(coleccion, campos, base_de_datos, dato)
+
+
+def validacion_simple_general_negativa(base_de_datos, dato):
+    coleccion = getattr(db_client, base_de_datos)
+    
+    if base_de_datos not in listas_de_campos:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"No hay definición de campos para la colección {base_de_datos}")
+    
+    campos = listas_de_campos[base_de_datos]
+    
+    if isinstance(dato, str):
+        validacion_simple_negativa_str(coleccion, campos, base_de_datos, dato)
+    else:
+        validacion_simple_negativa_no_str(coleccion, campos, base_de_datos, dato)
+
+
+def validacion_simple_str(coleccion, campos, base_de_datos, dato):
     for campo in campos:
-        query = (
-            {campo: {"$regex": f"^{dato}$", "$options": "i"}} if isinstance(dato, str)
-            else {campo: dato}
-        )
+        query = {
+            campo: {"$regex": f"^{dato}$", "$options": "i"}
+             }
         if coleccion.find_one(query):
-            raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
-                detail=f"El dato '{dato}' ya se encuentra en la base de datos '{base_de_datos}' en el campo '{campo}'"
-            )
-        
-        
-def validacion_simple_negativa_str(base_de_datos, dato):
-    coleccion = getattr(db_client, base_de_datos)
-    
-    if base_de_datos not in listas_de_campos:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"No hay definición de campos para la colección {base_de_datos}")
-    
-    campos = listas_de_campos[base_de_datos]
-    
+            errores_simples.error_simple_positivo(dato, base_de_datos, campo)
+            
+def validacion_simple_no_str(coleccion, campos, base_de_datos, dato):
     for campo in campos:
-        query = (
-            {campo: {"$regex": f"^{dato}$", "$options": "i"}} if isinstance(dato, str)
-            else {campo: dato}
-        )
+        query = {
+            campo:dato
+        }
+        if coleccion.find_one(query):
+            errores_simples.error_simple_positivo(dato, base_de_datos, campo)
+        
+        
+def validacion_simple_negativa_str(coleccion, campos, base_de_datos, dato):
+    for campo in campos:
+        query = {
+            campo: {"$regex": f"^{dato}$", "$options": "i"}
+            }
+        if  coleccion.find_one(query):
+            return
+                
+    errores_simples.error_simple_negativo(dato, base_de_datos)
+                
+
+def validacion_simple_negativa_no_str(coleccion, campos, base_de_datos, dato):
+    for campo in campos:
+        query = {
+            campo: dato
+            }
         if coleccion.find_one(query):
             return
-        
-    raise HTTPException(
-        status_code=status.HTTP_409_CONFLICT,
-        detail=f"El dato '{dato}' no se encuentra en la base de datos '{base_de_datos}'"
-        )
+                
+    errores_simples.error_simple_negativo(dato, base_de_datos)
+                
