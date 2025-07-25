224956bc57685a3c0ad534039a76c9be44938738
lazaro20 <salvirialazaro@gmail.com>
2025-07-25 15:17:59 -0300

    Feat(validaciones/validar_equipo.py): Agregar las validaciones por medio de otro archivo.

--- validaciones/validar_equipo.py
+++ validaciones/validar_equipo.py
@@ -1,26 +1,22 @@
-from db.client import db_client
 from fastapi import HTTPException, status
-
+from validaciones_generales import validaciones_generales_dobles
 
 def validacion_carga_equipo(datos, base_de_datos):
     if isinstance(datos, list) and len(datos) >= 2:
         equipos = set()
         for dato in datos:
             
-            key = validacion_carga_equipo_2(dato)
+            key = validacion_carga_equipo_2(dato, base_de_datos)
             
             if key in equipos:
                 raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Mismo Equipo ingresado 2 veces")
             equipos.add(key)
     else:
         dato = datos if not isinstance(datos, list) else datos[0]
-        key = validacion_carga_equipo_2(dato)
+        key = validacion_carga_equipo_2(dato, base_de_datos)
         
-def validacion_carga_equipo_2(dato):
+def validacion_carga_equipo_2(dato, base_de_datos):
     key = (dato.nombre_equipo.lower(), dato.pais_equipo.lower())
-    
-    if db_client.Equipos.find_one({"nombre_equipo":{"$regex": f"^{dato.nombre_equipo}$", "$options": "i"}, "pais_equipo": {"$regex": f"^{dato.pais_equipo}$", "$options": "i"}}):
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"El Equipo {dato.nombre_equipo} ya está cargado")
-    
+    validaciones_generales_dobles.validacion_doble_general(base_de_datos, dato.nombre_equipo, dato.pais_equipo)
     return key
         
