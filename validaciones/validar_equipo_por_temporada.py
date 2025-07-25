654ec4f10d6ac3dbe3a55792ae64d78811bbf258
lazaro20 <salvirialazaro@gmail.com>
2025-07-25 15:34:56 -0300

    Feat(validaciones/validar_equipo_por_temporada.py): Agregar las validaciones simples y dobles en el archivo por medio de importaciones.

--- validaciones/validar_equipo_por_temporada.py
+++ validaciones/validar_equipo_por_temporada.py
@@ -1,14 +1,14 @@
 from db.client import db_client
 from fastapi import HTTPException, status
 from funciones import funciones_logicas
+from validaciones_generales import validaciones_generales_dobles, validaciones_generales_simples
 
         
 def validar_carga_equipo_por_temporada(datos, base_de_datos):
-    coleccion = getattr(db_client, base_de_datos)
     if isinstance(datos, list) and len(datos) >= 2:
         equipos = set()
         for dato in datos:
-            key = validar_carga_equipo_por_temporada_2(dato, coleccion, datos)
+            key = validar_carga_equipo_por_temporada_2(dato, base_de_datos, datos)
             if key in equipos:
                 raise HTTPException(
                     status_code=status.HTTP_409_CONFLICT,
@@ -17,10 +17,10 @@ def validar_carga_equipo_por_temporada(datos, base_de_datos):
             equipos.add(key)
     else:
         dato = datos if not isinstance(datos, list) else datos[0]
-        validar_carga_equipo_por_temporada_2(dato, coleccion, datos)
+        validar_carga_equipo_por_temporada_2(dato, base_de_datos, datos)
 
 #Funcion para evitar la duplicidad de la carga de documentos de Equipos por temporada
-def validar_carga_equipo_por_temporada_2(dato, coleccion, datos):
+def validar_carga_equipo_por_temporada_2(dato, base_de_datos, datos):
         temporada_oid = funciones_logicas.validate_object_id(dato.temporada)
         equipo_oid = funciones_logicas.validate_object_id_or_false(dato.nombre_equipo)
         equipo = db_client.Equipos.find_one({"nombre_equipo":dato.nombre_equipo})
@@ -33,21 +33,17 @@ def validar_carga_equipo_por_temporada_2(dato, coleccion, datos):
         key = (temporada_oid, equipo_oid)
             
         season = db_client.Temporadas.find_one({"_id": temporada_oid})
-                    
-        if not db_client.Temporadas.find_one({"_id": temporada_oid}):
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Temporada incorrecta")
-            
-        if not db_client.Equipos.find_one({"_id":equipo_oid}):
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Equipo incorrecto (no encontrado en la base de datos)")
-            
-        if coleccion.find_one({"temporada": temporada_oid, "nombre_equipo":equipo_oid}):
-            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Equipo ya ingresado en la temporada")
         
-        limitacion_cantidad_por_temporada(season, temporada_oid, datos, coleccion)
+        validaciones_generales_simples.validacion_simple_general_negativa("Temporadas", temporada_oid)
+        validaciones_generales_simples.validacion_simple_general_negativa("Equipos", equipo_oid)
+        validaciones_generales_dobles.validacion_doble_general(base_de_datos, temporada_oid, equipo_oid)
+        
+        limitacion_cantidad_por_temporada(season, temporada_oid, datos, base_de_datos)
         
         return key
         
-def limitacion_cantidad_por_temporada(season, temporada_oid, datos, coleccion):
+def limitacion_cantidad_por_temporada(season, temporada_oid, datos, base_de_datos):
+        coleccion = getattr(db_client, base_de_datos)
         cantidad_actual = coleccion.count_documents({"temporada": temporada_oid})
         
         cantidad_maxima = season.get("cantidad_de_equipos")
