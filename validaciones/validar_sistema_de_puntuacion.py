68ef03df723840bef2970f871df115ee8d518f52
lazaro20 <salvirialazaro@gmail.com>
2025-07-29 13:34:19 -0300

    Refactor(validaciones/validar_sistema_de_puntuacion.py): Reorganizar el codigo en funciones mas chicas permitiendo la modularizacion.

--- validaciones/validar_sistema_de_puntuacion.py
+++ validaciones/validar_sistema_de_puntuacion.py
@@ -1,32 +1,45 @@
 from db.client import db_client
 from fastapi import HTTPException, status
 from funciones import funciones_logicas
-from validaciones_generales import validaciones_generales_dobles, validaciones_generales_simples
+from validaciones_generales import validaciones_generales_simples
 
         
-def validar_carga_sistema_de_puntuacion(datos, base_de_datos):
+def validar_carga_sistema_de_puntuacion(datos):
     if isinstance(datos, list) and len(datos) >= 2:
         puntos = set()
         for dato in datos:
-            key = validar_carga_sistema_de_puntuacion_2(dato, base_de_datos)
-            if key in puntos:
-                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Sistema de puntos duplicado en la entrega")
+            key, temporada = validar_carga_sistema_de_puntuacion_2(dato)
+            verificar_entrega_duplicada(dato, key, puntos, temporada)
             puntos.add(key)
     else:
     
         dato = datos if not isinstance(datos, list) else datos[0]
-        key  = validar_carga_sistema_de_puntuacion_2(dato, base_de_datos)
+        key  = validar_carga_sistema_de_puntuacion_2(dato)
         
 
-def validar_carga_sistema_de_puntuacion_2(dato, base_de_datos):
+def verificar_entrega_duplicada(dato, key, puntos, temporada):
+            if key in puntos:
+                raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Esta sistema de puntos = (posicion = '{dato.posicion}', puntos = '{dato.puntos}', tipo de carrera = {dato.tipo_carrera}) esta repetido en la entrega para la temporada = '{temporada['descripcion']}'")
+
+def validar_carga_sistema_de_puntuacion_2(dato):
     temporada_oid = funciones_logicas.validate_object_id(dato.temporada)
-    key = (dato.posicion, dato.puntos, dato.tipo_carrera.capitalize(), dato.temporada)
     
+    key = create_key(dato)
+    season = buscar_data(temporada_oid)
     validaciones_generales_simples.validacion_simple_general_negativa("Temporadas", temporada_oid)
-    
+    validacion_sistema_de_puntuacion(dato, temporada_oid, season)
 
-    if db_client.Sistema_de_puntuacion.find_one({"posicion": dato.posicion, "puntos": dato.puntos, "temporada":temporada_oid, "tipo_carrera": dato.tipo_carrera}):
-        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Sistema de puntos ya ingresados en Base de datos")
-    
+    return key, season
+
+def create_key(dato):
+    key = (dato.posicion, dato.puntos, dato.tipo_carrera.capitalize(), dato.temporada)
     return key
-        
+
+def buscar_data(temporada_oid):
+    season = db_client.Temporadas.find_one({"_id":temporada_oid})
+    return season
+
+def validacion_sistema_de_puntuacion(dato, temporada_oid, season):
+    if db_client.Sistema_de_puntuacion.find_one({"posicion": dato.posicion, "puntos": dato.puntos, "temporada":temporada_oid, "tipo_carrera": dato.tipo_carrera}):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"Esta sistema de puntos = (posicion = '{dato.posicion}', puntos = '{dato.puntos}', tipo de carrera = {dato.tipo_carrera}) ya se encuentra en la base de datos para la temporada = '{season['descripcion']}'")
+    
