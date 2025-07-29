ae88a60b19f9f132df30837eae115fb12ea77824
lazaro20 <salvirialazaro@gmail.com>
2025-07-29 13:21:36 -0300

    Refactor(validaciones/validar_pilotos_por_temporada.py): Reorganizar el codigo en funciones mas chicas permitiendo la modularizacion.

--- validaciones/validar_pilotos_por_temporada.py
+++ validaciones/validar_pilotos_por_temporada.py
@@ -9,35 +9,50 @@ def validar_carga_piloto_por_temporada(datos, base_de_datos):
         pilotos = set()
         for dato in datos:
             key = validar_carga_piloto_por_temporada_2(dato, base_de_datos)
-            if key in pilotos:
-                raise HTTPException(
-                    status_code=status.HTTP_409_CONFLICT,
-                    detail="piloto duplicado en la entrega"
-                )
+            verificar_entrega_duplicada(dato, key, pilotos)
             pilotos.add(key)
     else:
         dato = datos if not isinstance(datos, list) else datos[0]
         validar_carga_piloto_por_temporada_2(dato, base_de_datos)
 
+
+def verificar_entrega_duplicada(dato, key, pilotos):
+    if key in pilotos:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"el piloto = {dato.piloto_participante} duplicado en la entrega"
+        )
+
 #Funcion para evitar la duplicidad de la carga de documentos de pilotos por temporada
 def validar_carga_piloto_por_temporada_2(dato, base_de_datos):
+    temporada_oid, piloto_oid = buscar_oids(dato)
+    piloto = buscar_data(dato)
+    validar_piloto(piloto, piloto_oid)
+    key = create_key(piloto, temporada_oid)
+    validaciones_simples(temporada_oid, piloto_oid)
+    validaciones_generales_dobles.validacion_doble_general(base_de_datos, temporada_oid, piloto_oid)
+    return key
+
+def create_key(piloto, temporada_oid):
+    if piloto:
+        dict_piloto = dict(piloto)
+        piloto_oid = funciones_logicas.validate_object_id(dict_piloto["_id"])
+    key = (temporada_oid, piloto_oid)
+    return key
+
+def validar_piloto(piloto, piloto_oid):
+    if not piloto and not piloto_oid:
+        raise HTTPException(status_code=400, detail="Piloto no válido")
+
+def buscar_data(dato):
+    piloto  = db_client.Pilotos.find_one({"piloto_participante":dato.piloto_participante})
+    return piloto
+
+def buscar_oids(dato):
         temporada_oid = funciones_logicas.validate_object_id(dato.temporada)
         piloto_oid    = funciones_logicas.validate_object_id_or_false(dato.piloto_participante)
-        piloto        = db_client.Pilotos.find_one({"piloto_participante":dato.piloto_participante})
+        return temporada_oid, piloto_oid
         
-        if not piloto and not piloto_oid:
-            raise HTTPException(status_code=400, detail="Piloto no válido")
-        
-        if piloto:
-            dict_piloto = dict(piloto)
-            piloto_oid = funciones_logicas.validate_object_id(dict_piloto["_id"])
-        key = (temporada_oid, piloto_oid)
-                    
+def validaciones_simples(temporada_oid, piloto_oid):
         validaciones_generales_simples.validacion_simple_general_negativa("Temporadas", temporada_oid)
-        
         validaciones_generales_simples.validacion_simple_general_negativa("Pilotos", piloto_oid)
-        
-        validaciones_generales_dobles.validacion_doble_general(base_de_datos, temporada_oid, piloto_oid)
-
-        return key
-        
