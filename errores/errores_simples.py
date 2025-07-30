75a0afffc8a33e47225365fc14c9fc15f5277851
lazaro20 <salvirialazaro@gmail.com>
2025-07-30 09:57:41 -0300

    Feat(errores/errores_simples.py): Crear dos funciones de retorno de errores, error sin documento en la base de datos y error sin oid.

--- errores/errores_simples.py
+++ errores/errores_simples.py
@@ -18,3 +18,16 @@ def error_sin_base_de_datos(base_de_datos):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=f"No hay definición de campos para la colección {base_de_datos}"
         )
+
+
+def error_sin_oid(dato, base_de_datos):
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT, 
+        detail=f"El dato {dato} no fue encontrado en la base de datos {base_de_datos} con ningun documentos, por ende no podemos retornar un ID del mismo.")
+    
+    
+def error_sin_documentos_en_la_base_de_datos(dato, base_de_datos):
+    raise HTTPException(
+        status_code=status.HTTP_409_CONFLICT, 
+        detail=f"El dato {dato} no fue encontrado en la base de datos {base_de_datos} con ningun documentos, por ende no podemos dar retorno por sobre el mismo.")
+    
