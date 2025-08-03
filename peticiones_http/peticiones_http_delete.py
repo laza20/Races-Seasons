64a9626730b06409db3a8e28f2799bf0b4ff9ee1
lazaro20 <salvirialazaro@gmail.com>
2025-08-03 18:48:47 -0300

    Feat(peticiones_http/peticiones_http_delete.py): Crear una peticion delete la cual se encargue de borrar todos los datos de carrera. Al utilizar la funcion se eliminan los datos de carreras, carreras_por_equipo y carreras_por_piloto simultaneamente. Esto se hace por medio de los datos enviados en la url, los cuales en este caso son necesarios: {temporada}/{fecha}/{tipo_carrera}.

--- peticiones_http/peticiones_http_delete.py
+++ peticiones_http/peticiones_http_delete.py
@@ -34,3 +34,18 @@ def delete_many_by_data_str(router, base_de_datos, lista_datos):
         
         if encontrado != "True":   
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f"No se ah encontrado ningun dato {data}, con la propiedad {dato} en los documentos {base_de_datos}")
+        
+#si elimina una carrera busca los documentos relacionados con esa carrera en las otras tablas
+#y los elimina, temporada, fecha y tipo_carrera
+def delete_data_by_races(router, base_de_datos):
+    @router.delete("/Borrar/Datos/Carrera/{temporada}/{fecha}/{tipo_carrera}", status_code=status.HTTP_204_NO_CONTENT)
+    async def delete_races_many_diferents_documents(temporada:str, fecha:str, tipo_carrera:str):
+        coleccion = getattr(db_client, base_de_datos)
+        temporada_oid = funciones_logicas.validate_object_id(temporada)
+        borrado_carrera = coleccion.delete_many({"temporada":temporada_oid, "fecha":fecha, "tipo_carrera":{"$regex": f"^{tipo_carrera}$", "$options": "i"}})
+        borrado_carrera_piloto = db_client.Carreras_por_pilotos.delete_many({"temporada":temporada_oid, "fecha":fecha, "tipo_carrera":{"$regex": f"^{tipo_carrera}$", "$options": "i"}})
+        borrado_carrera_equipos = db_client.Carreras_por_equipos.delete_many({"temporada":temporada_oid, "fecha":fecha, "tipo_carrera":{"$regex": f"^{tipo_carrera}$", "$options": "i"}})
+        
+        if not  borrado_carrera and borrado_carrera_equipos and borrado_carrera_piloto:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Los datos ingresados para eliminar de las 3 tablas no fueron correctos.")
+        
