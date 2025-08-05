44f063806fcacc12e32f19a65177f90ca25b3cf1
lazaro20 <salvirialazaro@gmail.com>
2025-08-05 19:56:44 -0300

    Feat(funciones/funciones_podios.py): Agregar dos funciones, una para buscar_id_piloto_o_equipo y otra para verificar_existencia_dato_en_temporada ademas de un nuevo transformacion el cual se encarga de los datos los cuales no son de temporada con sus respectivos armados de dicts.

--- funciones/funciones_podios.py
+++ funciones/funciones_podios.py
@@ -3,19 +3,57 @@ from errores import errores_simples
 from funciones import funciones_logicas
 from fastapi import HTTPException, status
 
-
-def transformacion(base_de_datos, podios, dict_dato, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid):
+def transformacion_total(base_de_datos, podios, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid):
     if base_de_datos ==  "Carreras_por_pilotos":
-        podios =   transformacion_piloto(podios, dict_dato, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid)
+        podios =   transformacion_pilotos_total(podios, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid)
     else:
-        podios =  transformacion_equipo(podios, dict_dato, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid)
+        podios =   transformacion_equipos_total(podios, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid)
         
     return podios
 
+def transformacion_temporada(base_de_datos, podios, dict_dato, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid):
+    if base_de_datos ==  "Carreras_por_pilotos":
+            podios =   transformacion_piloto_temporada(podios, dict_dato, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid)
+    else:
+        podios =  transformacion_equipo_temporada(podios, dict_dato, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid)
+    return podios
+
+def inicializar_podios_totales():
+        podiums_totales = 0
+        podiums_primero = 0
+        podiums_segundo = 0
+        podiums_tercero = 0 
+        return podiums_totales, podiums_primero, podiums_segundo, podiums_tercero
 
 
 
-def transformacion_equipo(podios, equipo, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid):
+def transformacion_equipos_total(podios, primer_lugar, segundo_lugar, tercer_lugar, dato, categoria):
+    podios_equipo = {}
+    podios_equipo = {
+        "nombre_equipo"         : dato,
+        "podios_equipo"         : podios,
+        "primer_lugar"          : primer_lugar,
+        "segundo_lugar"         : segundo_lugar,
+        "tercer_lugar"          : tercer_lugar,
+        "categoria"             : categoria
+    }
+    return podios_equipo
+
+def transformacion_pilotos_total(podios, primer_lugar, segundo_lugar, tercer_lugar, dato, categoria):
+    podios_piloto = {}
+    podios_piloto = {
+        "nombre_piloto"         : dato,
+        "podios_piloto"         : podios,
+        "primer_lugar"          : primer_lugar,
+        "segundo_lugar"         : segundo_lugar,
+        "tercer_lugar"          : tercer_lugar,
+        "categoria"             : categoria
+            }
+    
+    return podios_piloto
+
+
+def transformacion_equipo_temporada(podios, equipo, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid):
     podios_equipo = {}
     temporada_actual = db_client.Temporadas.find_one({"_id": temporada_oid})
     podios_equipo = {
@@ -31,7 +69,7 @@ def transformacion_equipo(podios, equipo, primer_lugar, segundo_lugar, tercer_lu
     }
     return podios_equipo
 
-def transformacion_piloto(podios, piloto, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid):
+def transformacion_piloto_temporada(podios, piloto, primer_lugar, segundo_lugar, tercer_lugar, dato, temporada_oid):
     podios_piloto = {}
     temporada_actual = db_client.Temporadas.find_one({"_id":temporada_oid})
     podios_piloto = {
@@ -73,6 +111,9 @@ def buscar_podios(carreras_totales, puntos_por_posicion, base_de_datos):
     return primer_lugar, segundo_lugar, tercer_lugar, podios
 
 
+#retorna el el campo_carrera el cual sera utilizado para verificar si un piloto o equipo estan
+#dentro de la base de datos de carreras_por_piloto o carreras_por_equipo, ademas retorna un dict del piloto o equipo
+#que puede ser usado para sacar la informacion de este
 def buscar_data_team_or_driver(base_de_datos, dato, temporada_oid):
     coleccion_uno, coleccion_dos, campo, campo_carrera =  seleccionar_piloto_o_equipo(base_de_datos)
     dato_encontrado = coleccion_dos.find_one({campo:dato}) #busca en la base de datos de pilotos o equipos
@@ -88,8 +129,41 @@ def buscar_data_team_or_driver(base_de_datos, dato, temporada_oid):
     if not participante:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Piloto {dato}, {campo} no encontrado en la temporada")
     
-    return participante,campo_carrera
+    #retorna el dict de pilotos o equipos por temporada y el campo que servira para verificar los datos.
+    return participante, campo_carrera
 
+
+#el piloto_participante y nombre_equipo en pilotos_por_temporada y equipos_por_temporada
+#son ids por eso esta funcion se encarga de buscar esos ids y retornarlos como nombres para poder usarlos
+#en la verificacion. lo hace por medio del nombre (tanto del equipo como del piloto)
+def buscar_id_piloto_o_equipo(base_de_datos, dato):
+    if base_de_datos == "Carreras_por_pilotos":
+        piloto = db_client.Pilotos.find_one({"piloto_participante":{"$regex": f"^{dato}$", "$options": "i"}})
+        if not piloto:
+            errores_simples.error_sin_documentos_en_la_base_de_datos(dato, base_de_datos)
+        dato_oid = funciones_logicas.validate_object_id(piloto["_id"])
+    else:
+        equipo = db_client.Equipos.find_one({"nombre_equipo":{"$regex": f"^{dato}$", "$options": "i"}})
+        if not equipo:
+            errores_simples.error_sin_documentos_en_la_base_de_datos(dato, base_de_datos)
+        dato_oid = funciones_logicas.validate_object_id(equipo["_id"])
+    
+    return dato_oid
+
+#verifica si un dato esta en la temporada que se envie.
+def verificar_existencia_dato_en_temporada(base_de_datos, dato_oid, temporada_oid):
+    if base_de_datos == "Carreras_por_pilotos":
+        piloto = db_client.Pilotos_por_temporada.find_one({"piloto_participante":dato_oid,
+                                                            "temporada": temporada_oid})
+        if not piloto:
+            return "false"
+    else:
+        equipo = db_client.Equipos_por_temporada.find_one({"nombre_equipo":dato_oid,
+                                                            "temporada": temporada_oid})
+        if not equipo:
+            return "false"
+
+#dependiende la base de datos selecciona una serie de campos.
 def seleccionar_piloto_o_equipo(base_de_datos):
     if base_de_datos == "Carreras_por_pilotos":
         coleccion_uno = getattr(db_client, "Pilotos_por_temporada")
