9d0a80462a8239c9437cd5d44b9c6f4a7d265d98
lazaro20 <salvirialazaro@gmail.com>
2025-08-06 08:55:35 -0300

    Refactor(main.py): Eliminar el router de datos_carreras ya que el archivo fue eliminado.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from routers import (
     carreras, 
     conformacion_de_equipos,
     tabla_posiciones, 
-    datos_carreras, 
     temporada, 
     equipos_por_temporada, 
     pilotos_por_temporada, 
@@ -28,7 +27,6 @@ app.include_router(sistema_de_puntuacion.router)
 app.include_router(conformacion_de_equipos.router)
 app.include_router(carreras.router)
 app.include_router(tabla_posiciones.router)
-app.include_router(datos_carreras.router)
 app.include_router(temporada.router)
 app.include_router(equipos_por_temporada.router)
 app.include_router(pilotos_por_temporada.router)
